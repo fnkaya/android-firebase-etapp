d915eb56b2f189aaebb6c8a22e2f762020f09d32
Furkan Kaya <fnkaya94@gmail.com>
2021-02-27 23:38:08 +0300

    Some dependencies inserted

--- app/src/main/java/com/gazitf/etapp/MainActivity.java
+++ app/src/main/java/com/gazitf/etapp/MainActivity.java
@@ -2,11 +2,13 @@ package com.gazitf.etapp;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,11 +23,15 @@ public class MainActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
 
+        ImageView imageViewProfile = findViewById(R.id.image_view_profile);
         TextView textUser = findViewById(R.id.text_user);
-
-        textUser.setText(auth.getCurrentUser().getDisplayName() + " " + auth.getCurrentUser().getEmail() + " " + auth.getCurrentUser().getPhoneNumber());
-
         Button btnSignOut = findViewById(R.id.button_sign_out);
+
+        Picasso.get()
+                .load(auth.getCurrentUser().getPhotoUrl())
+                .into(imageViewProfile);
+
+        textUser.setText(auth.getCurrentUser().getDisplayName() + " " + auth.getCurrentUser().getEmail() + " " + auth.getCurrentUser().getUid());
 
         btnSignOut.setOnClickListener(view -> {
             auth.signOut();
