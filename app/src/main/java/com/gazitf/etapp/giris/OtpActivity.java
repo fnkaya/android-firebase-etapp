c9d5387618ab72194bd4c7c4f953f3c4b3e5c799
Furkan Kaya <fnkaya94@gmail.com>
2021-02-28 18:24:40 +0300

    Phone authentication bug fixed

--- app/src/main/java/com/gazitf/etapp/giris/OtpActivity.java
+++ app/src/main/java/com/gazitf/etapp/giris/OtpActivity.java
@@ -1,6 +1,7 @@
 package com.gazitf.etapp.giris;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +10,8 @@ import android.widget.Toast;
 
 import com.gazitf.etapp.MainActivity;
 import com.gazitf.etapp.R;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,6 +20,7 @@ import com.google.firebase.auth.PhoneAuthProvider;
 
 public class OtpActivity extends AppCompatActivity {
 
+    private ConstraintLayout constraintLayoutOtp;
     private TextInputLayout textInputLayoutVerificationCode;
     private TextInputEditText textInputVerificationCode;
     private Button buttonVerify;
@@ -29,6 +33,7 @@ public class OtpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
 
+        constraintLayoutOtp = findViewById(R.id.constraint_layout_otp);
         textInputLayoutVerificationCode = findViewById(R.id.text_input_layout_verification_code);
         textInputVerificationCode = findViewById(R.id.text_input_verification_code);
         buttonVerify = findViewById(R.id.button_verify);
@@ -43,12 +48,13 @@ public class OtpActivity extends AppCompatActivity {
         buttonVerify.setOnClickListener(view -> {
             String verification_code = textInputVerificationCode.getText().toString();
 
-            if (!verification_code.isEmpty()){
+            if (verification_code.length() == 6){
+                textInputLayoutVerificationCode.setError(null);
                 PhoneAuthCredential credential = PhoneAuthProvider.getCredential(otp, verification_code);
                 authWithPhoneNumber(credential);
             }
             else{
-                textInputLayoutVerificationCode.setError("Please enter OTP");
+                textInputLayoutVerificationCode.setError("Please enter verification code");
             }
         });
     }
@@ -60,7 +66,7 @@ public class OtpActivity extends AppCompatActivity {
                     this.finishAffinity();
                 })
                 .addOnFailureListener(error -> {
-                    Toast.makeText(this, error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                    Snackbar.make(constraintLayoutOtp, error.getLocalizedMessage(), Snackbar.LENGTH_LONG).show();
                 });
     }
 }
